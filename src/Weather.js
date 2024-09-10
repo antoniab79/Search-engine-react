bdd14a6e7a94f087c216083efc2145d1636ede3e
antoniab79 <antonia@webleviathan.com>
2024-09-10 19:33:38 +0100

    rounded temp in weather

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Weather({ data }) {
   return (
     <ul>
-      <li>Temperature: {data.temperature}°C</li>
+      <li>Temperature: {Math.round(data.temperature)}°C</li>
       <li>Conditions: {data.conditions}</li>
       <li>Humidity: {data.humidity}%</li>
       <li>Wind Speed: {data.wind} m/s</li>
