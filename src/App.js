936c7ee199f3ba3e24d080b683346cdcc308d34d
antoniab79 <antonia@webleviathan.com>
2024-09-11 19:51:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import "./App.css";
+import React from "react";
 import SearchEngine from "./SearchEngine";
+import "./App.css";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <h1>Weather Search Engine</h1>
-      <SearchEngine />
+      <header className="App-header">
+        <h1>Weather SearchEngine</h1>
+        <SearchEngine />
+        <p>
+          This code is{" "}
+          <a
+            className="App-link"
+            href="https://github.com/antoniab79/Search-engine-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open sourced
+          </a>
+          , coded by{" "}
+          <a
+            className="App-link"
+            href="https://www.shecodes.io/graduates/109902-antonia-bezinovic"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Antonia Bezinovic
+          </a>{" "}
+          and is on{" "}
+          <a
+            className="App-link"
+            href="https://www.shecodes.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+        </p>
+      </header>
     </div>
   );
 }
+
+export default App;
