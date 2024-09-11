d3116f8726d2e3ea15c26453a604d0dd46df4669
antoniab79 <antonia@webleviathan.com>
2024-09-11 19:34:22 +0100

    added github link

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,6 +8,35 @@ function App() {
       <header className="App-header">
         <h1>Weather SearchEngine</h1>
         <SearchEngine />
+        <p>
+          This code is{" "}
+          <a
+            className="App-link"
+            href="https://github.com/antoniab79/Search-engine-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open sourced
+          </a>
+          , coded by{" "}
+          <a
+            className="App-link"
+            href="https://www.shecodes.io/graduates/109902-antonia-bezinovic"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Antonia Bezinovic
+          </a>{" "}
+          and is on{" "}
+          <a
+            className="App-link"
+            href="https://www.shecodes.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+        </p>
       </header>
     </div>
   );
